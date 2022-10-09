c0d0694091b8d9f8a44764d7c85f8378f321f08b
Sebastian Prieto <35538199+SebaGPDev@users.noreply.github.com>
2022-10-09 09:07:39 -0300

    changes

--- src/database.js
+++ src/database.js
@@ -1,14 +1,9 @@
 const mongoose = require("mongoose");
 
-//Set up default mongoose connection
-const {MONGODB_HOST, MONGODB_DATABASE} = process.env;
-const mongoDB = `mongodb://${MONGODB_HOST}/${MONGODB_DATABASE}`;
+const { MONGODB_HOST, MONGODB_DATABASE } = process.env;
+const MONGODB_URI = `mongodb://${MONGODB_HOST}/${MONGODB_DATABASE}`;
 
 mongoose
-  .connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
   .then((db) => console.log("database connection established"))
   .catch((err) => console.log("connection error: " + err.message));
-
-//Get the default connection
-const db = mongoose.connection;
-module.exports = db;
